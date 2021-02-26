3222ec7fa1a60b22106ea7d400f344e1b9bb7221
Jefferson <jefferson2001santos@gmail.com>
2021-02-26 14:52:47 -0300

    Revert "Desabilitar menu na tela inicial"
    This reverts commit dbc055e1d067e9a1e725636104f0b7df48470def.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
-import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -9,18 +8,9 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public menu :MenuController) {
+  constructor(public navCtrl: NavController) {
 
   }
-  ionViewWillEnter() {
-    this.menu.swipeEnable(false);
-    }
-    
-  ionViewDidLeave() {
-    this.menu.swipeEnable(true);
-    }
-
-
   login(){
     this.navCtrl.setRoot('CategoriasPage');
   }
