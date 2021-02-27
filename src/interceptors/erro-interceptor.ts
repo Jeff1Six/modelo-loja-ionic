ae065e40bd4e23ea1a00e71d3d2abe661cdc9765
Jefferson <jefferson2001santos@gmail.com>
2021-02-27 01:33:36 -0300

    tratamento padrao para outros erros com alert

--- src/interceptors/erro-interceptor.ts
+++ src/interceptors/erro-interceptor.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { AlertController } from "ionic-angular";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 
@@ -7,7 +8,7 @@ import { StorageService } from "../services/storage.service";
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){}
+    constructor(public storage: StorageService, public alert: AlertController){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
@@ -23,9 +24,17 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch(errorObj.status){
+                    case 401:
+                        this.handle401();
+                        break;
+
                     case 403:
                         this.handle403();
                         break;
+                    
+                    default: 
+                        this.handlerDefaultError(errorObj);
+                   
                 }
 
                 return Observable.throw(errorObj);
@@ -34,6 +43,35 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+    handle401(){
+        let alert = this.alert.create({
+            title: 'Erro de Credenciais',
+            message: 'Email ou senha  incorretos',
+            enableBackdropDismiss: false, //Somente sair apertando no botao do alerta
+            buttons: [
+                {
+                    text: 'OK'
+                }
+
+            ]
+        });
+        alert.present()
+    }
+
+    handlerDefaultError(errorObj){
+        let alert = this.alert.create({
+            title: 'Erro  '+ errorObj.status + ': ' + errorObj.error ,
+            message: errorObj.message,
+            enableBackdropDismiss: false, //Somente sair apertando no botao do alerta
+            buttons: [
+                {
+                    text: 'OK'
+                }
+
+            ]
+        });
+        alert.present()
     }
     
 }
