a20e8c376f72c1d6fb5abbac41b040e81874d062
Jefferson <jefferson2001santos@gmail.com>
2021-03-02 14:56:14 -0300

    Armazenamento dos dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ClienteDTO } from '../../models/Cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.service';
 @IonicPage()
@@ -12,12 +13,14 @@ import { StorageService } from '../../services/storage.service';
 export class PickAddressPage {
 
   items: EnderecoDTO[];
+  pedido: PedidoDTO;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public cartService: CartService) {
   }
 
  
@@ -27,6 +30,15 @@ export class PickAddressPage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.items = response['enderecos'];
+          let cart = this.cartService.getCart();
+
+
+          this.pedido = {
+            cliente: {id: response['id']}, 
+              enderecoDeEntrega: null,
+              pagamento: null,
+              itens: cart.itens.map( x =>{ return {quantidade:x.quantidade, produto: {id: x.produto.id}}}) //Lambida para percorrer a lista 
+          }
         },  error => {
           if(error.status == 403 ){
             this.navCtrl.setRoot('HomePage');
@@ -35,9 +47,15 @@ export class PickAddressPage {
     }else {
       this.navCtrl.setRoot('HomePage');
     }
+
   }
 
+  nextPage(item: EnderecoDTO){
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido)
+  }
 
+}
 
    
-}
+
