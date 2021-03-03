48b6e4317e1ea0901335240baaa86b591aa48083
Jefferson <jefferson2001santos@gmail.com>
2021-03-03 15:01:52 -0300

    refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -21,6 +21,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData()
+  }
+
+  loadData(){
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -33,7 +37,6 @@ export class ProdutosPage {
         loader.dismiss();
       })
   }
-
   
   loadImageUrl(){
     for (var i=0; i<this.items.length; i++){
@@ -53,10 +56,15 @@ presentLoading(){
   return loader;
 }
 
-
-
 showDetail(produto_id : string){
   this.navCtrl.push('ProdutoDetailPage' , {produto_id: produto_id})
+}
+
+doRefresh(refresher) {
+  this.loadData();
+  setTimeout(() => {
+    refresher.complete();
+  }, 1000);
 }
 
 }
